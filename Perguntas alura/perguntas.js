32eb4d170f5df5b12b99c31499a83be7fe823766
Jeder8 <jeder@professor.educacao.sp.gov.br>
2024-10-17 08:45:29 -0300

    Add files via upload

---
+++ Perguntas alura/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Japão?',
+    'A capital da França é Tóquio'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu em Inglês?',
+    'Oi em ingles é I (AI)'
+)
